a00dfa3ad06f4bea9ae792961214ca8bd6829f8c
JorgeGutierrez7 <>
2021-04-26 16:49:13 +0000

    Mensaje de cambio

---
+++ areatriangulo.rb
@@ -0,0 +1,12 @@
+  
+#Variable y otro
+print "Ajercicio 01: Area de un Triangulo\n"
+#Datos de Entrada
+puts "Ingrese Base:"
+b=gets.to_i
+puts "Ingrese Altura:"
+h=gets.to_i
+#Procesos
+area=(b*h)/2
+#Datos
+puts "El area del Triangulo es: ", area
